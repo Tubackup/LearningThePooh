b8278752d58771d30aaa218cfb3495a1dae6dd3c
Fx <admin@52tutu.com.cn>
2020-07-16 18:57:45 +0800

    1.Automatically select the scan code to log in 2.move the QR code to the middle of the window

--- release/js/login.js
+++ release/js/login.js
@@ -1,4 +1,4 @@
-let qrCode = document.querySelector('.ddloginbox');
+/*let qrCode = document.querySelector('.ddloginbox');
 if (qrCode) {
     setTimeout(function () {
         qrCode.scrollIntoView({
@@ -7,11 +7,18 @@ if (qrCode) {
         setTimeout(function () {
             window.scrollTo({
                 left: (document.body.scrollWidth - document.documentElement.clientWidth) / 2,
-                top: window.scrollY,
                 behavior: 'smooth'
             });
         }, 1000 + Math.floor(Math.random() * 1000));
     }, 1000 + Math.floor(Math.random() * 1000))
-}
+}*/
+
+setTimeout(function () {
+  window.scrollTo({
+		top:927,
+		left: (document.body.scrollWidth - document.documentElement.clientWidth) / 2,
+		behavior: 'smooth'
+	});
+}, 1000 + Math.floor(Math.random() * 1000));
 
 chrome.runtime.sendMessage({"method": "checkLogin"});
